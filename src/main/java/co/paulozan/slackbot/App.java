73c43c4a3aed425fc62ccab0cf7e88f0dee115a6
Paulo Cesar Zanco <pzanco@Paulos-MacBook-Pro.local>
2017-06-10 17:36:54 -0300

    first commit

---
+++ src/main/java/co/paulozan/slackbot/App.java
@@ -0,0 +1,13 @@
+package co.paulozan.slackbot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
